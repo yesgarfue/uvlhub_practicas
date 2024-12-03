f7752e8b26003727010153a74fce7d2fe6f5a8bd
Yesica Garate <yesgarfue@alum.us.es>
2024-12-03 20:00:05 +0100

    Version modificada y corregida del workflow y documentacion en Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,17 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('../../'))  # Ruta al directorio del proyecto
-
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'uvlhub_practicas'
+project = 'Pruebas de workflow'
 copyright = '2024, Yesica Garate'
 author = 'Yesica Garate'
 
@@ -30,5 +23,5 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,15 +3,15 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-uvlhub_practicas documentation
-==============================
+Documentación del Proyecto
+==========================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+Bienvenidos a la documentación del proyecto de Pescaito-Hub
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: Contenidos:
 
+   miembros
+   milestones
+   mejoras
